5fce8946c804c0cd858a88524c8586106ded4584
lailikezs <lailikezs@gmail.com>
2025-07-23 01:53:59 +0800

    Site updated: 2025-07-23 01:53:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Article/hello-world/","Article/这是渣渣辉/"];function toRandomPost(){
+var posts=["Article/hello-world/","Article/渣渣辉/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
